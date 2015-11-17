0d48c652b9d2397f063b3cec72d152c56376fa2b
t04st3r <raffaele.tosti@studenti.unipg.it>
2015-11-17 12:48:15 +0800

    started transaction implementation backend side

--- db.php
+++ db.php
@@ -18,7 +18,6 @@ class db {
     private $password = 'Fin::Account::314';
     private $host = 'localhost';
     private $conn = null;
-    private $result;
 
     function __construct() {
         if (!isset($this->conn) || $this->conn == null) {
@@ -33,10 +32,6 @@ class db {
         if (isset($this->conn) || $this->conn != null) {
             unset($this->conn);
         }
-    }
-
-    private function error_message() {
-        return "Database Error: " . mysqli_error($this->conn);
     }
 
     public function checkLogin($user_name, $password, $matrix_array) {
@@ -54,10 +49,10 @@ class db {
         if (!$matrix_result) {
             return array('result' => false, 'matrix' => true);
         }
-        return array('result' => true , 'id' => $id);
+        return array('result' => true, 'id' => $id);
     }
-    
-    public function getUserNameSurname($id){
+
+    public function getUserNameSurname($id) {
         $query = "SELECT name, surname FROM customer WHERE customer_id = ?";
         $stmt = $this->conn->prepare($query);
         $stmt->bind_param('i', $id);
@@ -66,7 +61,7 @@ class db {
         $result_set = $result->fetch_array(MYSQLI_ASSOC);
         $name = $result_set['name'];
         $surname = $result_set['surname'];
-        return $name.' '.$surname;
+        return $name . ' ' . $surname;
     }
 
     private function checkMatrix($matrix_array, $id) {
@@ -78,8 +73,8 @@ class db {
         $row = $this->conn->affected_rows;
         return $row == 1;
     }
-    
-    public function getAccountCards($id){
+
+    public function getAccountCards($id) {
         $query = "SELECT account_number, balance, currency, account.card_number, type_name, card_circuit AS circuit "
                 . "FROM account, account_type, card WHERE customer_id = ? "
                 . "AND account_type = id_type "
@@ -91,8 +86,8 @@ class db {
         $result_set = $result->fetch_all(MYSQLI_ASSOC);
         return $result_set;
     }
-    
-    public function getAccountsNumber($id){
+
+    public function getAccountsNumber($id) {
         $query = "SELECT account_number FROM account WHERE customer_id = ? ";
         $stmt = $this->conn->prepare($query);
         $stmt->bind_param('i', $id);
@@ -100,6 +95,135 @@ class db {
         $result = $stmt->get_result();
         $result_set = $result->fetch_all(MYSQLI_ASSOC);
         return $result_set;
+    }
+
+    
+    public function transaction($cust_id, $cust_account, $amount, $ben_name, $ben_surname, $ben_account, $message, $ip) {
+
+        //check whether customer account and beneficiary account are the same
+        if (strcmp($cust_account, $ben_account) == 0) {
+            $error = 'customer account cannot be equal to beneficiary account';
+            $t_code = $this->writeTransactionLog($cust_account, $amount, $ben_account, $ip, $message, false, $error);
+            return array('result' => false,
+                'message' => 'Transaction Aborted:<br/>' . $error . '<br/>Transaction Code: ' . $t_code);
+        }
+        
+        //check wheter the value of accounts numbers and amounts are numeric
+        if (!is_numeric($amount) || !is_numeric($cust_account) || !is_numeric($ben_account)) {
+            $error = 'bad data format';
+            $t_code = $this->writeTransactionLog($cust_account, $amount, $ben_account, $ip, $message, false, $error);
+            return array('result' => false,
+                'message' => 'Transaction Aborted:<br/>' . $error . '<br/>Transaction Code: ' . $t_code);
+        }
+        
+        //check whether the amount is higher the minimum required
+        if ($amount < 100) {
+            $error = 'minimun amount transferable is 100.00 $HK';
+            $t_code = $this->writeTransactionLog($cust_account, $amount, $ben_account, $ip, $message, false, $error);
+            return array('result' => false,
+                'message' => 'Transaction Aborted:<br/>' . $error . '<br/>Transaction Code: ' . $t_code);
+        }
+
+        //check whether the amount is lover than the maximun funds transferable
+        if ($amount > 500000) {
+            $error = 'maximum amount transferable is 500,000.00 $HK, please contact your branch director for further information';
+            $t_code = $this->writeTransactionLog($cust_account, $amount, $ben_account, $ip, $message, false, $error);
+            return array('result' => false,
+                'message' => 'Transaction Aborted:<br/>' . $error . '<br/>Transaction Code: ' . $t_code);
+        }
+        
+        //check whether the customer account account exists
+        $result_cust_account = $this->checkAccountExists($cust_account);
+        if (!$result_cust_account['result']) {
+            $error = 'unknown customer account';
+            $t_code = $this->writeTransactionLog($cust_account, $amount, $ben_account, $ip, $message, false, $error);
+            return array('result' => false,
+                'message' => 'Transaction Aborted:<br/>' . $error . '<br/>Transaction Code: ' . $t_code);
+        }
+
+        //check whether the beneficiary account exists
+        $result_account_exists = $this->checkAccountExists($ben_account);
+        
+        if (!$result_account_exists['result']) {
+            $error = 'unknown beneficiary account';
+            $t_code = $this->writeTransactionLog($cust_account, $amount, $ben_account, $ip, $message, false, $error);
+            return array('result' => false,
+                'message' => 'Transaction Aborted:<br/>' . $error . '<br/>Transaction Code: ' . $t_code);
+        }
+        
+        
+        //check whether the beneficiary name is exactly the same of the registered one
+        $array_result = $result_account_exists['array'];
+        
+        if (strcasecmp($array_result['name'], $ben_name) != 0 || strcasecmp($array_result['surname'], $ben_surname) != 0 ) {
+            $error = 'unknown beneficiary name';
+            $t_code = $this->writeTransactionLog($cust_account, $amount, $ben_account, $ip, $message, false, $error);
+            return array('result' => false,
+                'message' => 'Transaction Aborted:<br/>' . $error . '<br/>Transaction Code: ' . $t_code);
+        }
+        
+        //check whether there is suffucent funds to perform the transation
+        $check_funds = $this->checkSufficientFunds($cust_id, $amount, $cust_account);
+        
+        if(!$check_funds){
+           $error = 'insufficient funds to perform the transaction';
+           $t_code = $this->writeTransactionLog($cust_account, $amount, $ben_account, $ip, $message, false, $error);
+            return array('result' => false,
+                'message' => 'Transaction Aborted:<br/>' . $error . '<br/>Transaction Code: ' . $t_code); 
+        }
+        
+        $id_beneficiary = $array_result['id'];
+        
+        return array('result' => true, 'message' => 'ok');
+    }
+
+    private function checkAccountExists($account) {
+        $query = "SELECT customer.customer_id AS id, name, surname FROM account, customer WHERE "
+                . "account_number = ? AND customer.customer_id = account.customer_id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param('i', $account);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $rows = $stmt->affected_rows;
+        if ($rows != 1) {
+            return array('result' => false);
+        } else {
+            $result_set = $result->fetch_array(MYSQLI_ASSOC);
+            return array('result' => true, 'array' => $result_set);
+        }
+    }
+    //write on the transaction table the successful or aborted transaction
+    private function writeTransactionLog($cust_account, $amount, $dest_account, $ip, $message, $flag = false, $error = NULL) {
+        $date = date('Y-m-d H:i:s');
+        $characters_to_eliminate = array('-', ':', ' ');
+        $characters_to_replace = array('', '', '');
+        //primary key of the transaction table are a string composed by the date (stripped by non-number characters) and customer account number 
+        $transaction_code = str_replace($characters_to_eliminate, $characters_to_replace, $date);
+        $flag_mysql = $flag ? 'executed' : 'aborted';
+        $query = "INSERT INTO transaction (transaction_code, operation_time_date, account, transaction_amount, flag, dest_account, ip_address, message, error) "
+                . "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param('ssiisisss', $transaction_code, $date, $cust_account, $amount, $flag_mysql, $dest_account, $ip, $message, $error);
+        $stmt->execute();
+        return $transaction_code;
+    }
+    
+    //check whether there is enough money on the customer account to perform the transaction
+    private function checkSufficientFunds($id, $amount, $customer_account) {
+        $query = "SELECT balance FROM account WHERE "
+                . "account_number = ? AND customer_id = ?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param('ii', $customer_account, $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $rows = $stmt->affected_rows;
+        if ($rows != 1) {
+            return false;
+        } else {
+            $result_set = $result->fetch_array(MYSQLI_ASSOC);
+            $cust_amount = $result_set['balance'];
+            return $cust_amount >= $amount;
+        }
     }
 
 }
--- transaction.php
+++ transaction.php
@@ -8,38 +8,60 @@ require_once './db.php';
 if (!isset($_SESSION['token']) || !isset($_SESSION['id']) || $_GET['token'] != $_SESSION['token']) {
     header('Location: http://' . $_SERVER['HTTP_HOST'] . '?err=auth');
 }
+
+
+if (isset($_POST['ben_account']) && isset($_POST['customer_account']) && isset($_POST['amount']) && isset($_POST['ben_name']) && isset($_POST['ben_surname']) && isset($_POST['ben_account'])) {
+
+    //filtering input data to prevent SQL injection
+    $cust_account = filter_input(INPUT_POST, 'customer_account', FILTER_SANITIZE_SPECIAL_CHARS);
+    $amount = filter_input(INPUT_POST, 'amount', FILTER_SANITIZE_SPECIAL_CHARS);
+    $ben_name = filter_input(INPUT_POST, 'ben_name', FILTER_SANITIZE_SPECIAL_CHARS);
+    $ben_surname = filter_input(INPUT_POST, 'ben_surname', FILTER_SANITIZE_SPECIAL_CHARS);
+    $ben_account = filter_input(INPUT_POST, 'ben_account', FILTER_SANITIZE_SPECIAL_CHARS);
+    $message = isset($_POST['message']) ? filter_input(INPUT_POST, 'message', FILTER_SANITIZE_SPECIAL_CHARS) : '';
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $cust_id = $_SESSION['id'];
+
+    $db = new db;
+    $result = $db->transaction($cust_id, $cust_account, $amount, $ben_name, $ben_surname, $ben_account, $message, $ip);
+}
+
+$error = !$result['result'];
+$message = $result['message'];
 ?>
 <!DOCTYPE html>
 <html>
     <head>
-        <?php
-        require_once './modules/header.php';
-        ?>
+<?php
+require_once './modules/header.php';
+?>
         <link rel="stylesheet" href="css/transaction.css">
     </head>
     <body>
-        <?php
-        require_once './modules/logo.php';
-        $id = $_SESSION['id'];
-        $db = new db();
-        $customer_name = $db->getUserNameSurname($id);
-        echo '<p id="user_message">Logged As: ' . $customer_name . ' <a href="/index.php?logout=true">Log Out</a></p>';
-        require_once './modules/menubar.php';
-        $account_number_array = $db->getAccountCards($id);
-        ?>
+<?php
+require_once './modules/logo.php';
+$id = $_SESSION['id'];
+$db = new db();
+$customer_name = $db->getUserNameSurname($id);
+echo '<p id="user_message">Logged As: ' . $customer_name . ' <a href="/index.php?logout=true">Log Out</a></p>';
+require_once './modules/menubar.php';
+$account_number_array = $db->getAccountCards($id);
+?>
         <div id="background">
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-lg-4 col-lg-offset-4 formContainer">
+                        <h4 class="<?php $class = $error ? 'error' : 'no_error';
+        echo $class; ?>"><?php echo $message; ?></h4>
                         <form method="POST">
                             <div class="form-group">
                                 <label for="selectAccount">Select Your Account</label>
-                                <select class="form-control" id="selectAccount">
-                                    <?php
-                                    foreach ($account_number_array as $key => $value) {
-                                        echo '<option value="' . $value['account_number'] . '">' . $value['account_number'] . '</option>';
-                                    }
-                                    ?>
+                                <select class="form-control" id="selectAccount" name="customer_account">
+<?php
+foreach ($account_number_array as $key => $value) {
+    echo '<option value="' . $value['account_number'] . '">' . $value['account_number'] . '</option>';
+}
+?>
                                 </select>
 
                             </div>
@@ -47,28 +69,36 @@ if (!isset($_SESSION['token']) || !isset($_SESSION['id']) || $_GET['token'] != $
                                 <label for="InputAmount">Amount (in Hong Kong dollar)</label>
                                 <div class="input-group">
                                     <div class="input-group-addon">$HK</div>
-                                    <input type="number" class="form-control" id="InputAmount" placeholder="Amount" min="100" max="50000">
+                                    <input type="number" name="amount" class="form-control" id="InputAmount" placeholder="Amount" min="100" max="500000" required>
                                     <div class="input-group-addon">.00</div>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="beneficiaryField">Beneficiary Name</label>
-                                <input type="text" class="form-control" id="beneficiaryField" placeholder="Beneficiary Name">
+                                <input type="text" name="ben_name" class="form-control" id="beneficiaryField" placeholder="Beneficiary Name" requred>
+                            </div>
+                            <div class="form-group">
+                                <label for="beneficiaryField">Beneficiary Surname</label>
+                                <input type="text" name="ben_surname" class="form-control" id="beneficiaryField" placeholder="Beneficiary Surname" required>
                             </div>
                             <div class="form-group">
                                 <label for="beneficiaryAccountField">Beneficiary Account</label>
-                                <input type="number" class="form-control" id="beneficiaryAccountField" placeholder="Beneficiary Account">
+                                <input type="number" name="ben_account" class="form-control" id="beneficiaryAccountField" placeholder="Beneficiary Account" required>
                             </div>
-                            
+                            <div class="form-group">
+                                <label for="beneficiaryAccountField">Message for the Beneficiary</label>
+                                <textarea name="message" class="form-control" rows="3"></textarea>
+                            </div>
+
                             <button type="submit" class="btn btn-primary btn-lg">Transfer Money</button>
                         </form>
                     </div>
                 </div> 
             </div>
         </div>
-        <?php
-        require_once './modules/footer.php';
-        ?>
+<?php
+require_once './modules/footer.php';
+?>
 
         <script src="js/jquery-2.1.4.min.js"></script>   
         <script src="js/bootstrap.min.js"></script>
